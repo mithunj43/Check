e43efe69d9d7dc61c3d5ab99ad2cc6a6a8c2d76f
mithun <mithunj43@gmail.com>
2018-12-20 23:25:23 +0530

    Initial commit
    Initial commit

---
+++ src/test/java/com/focus/check/CheckApplicationTests.java
@@ -0,0 +1,17 @@
+package com.focus.check;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CheckApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
